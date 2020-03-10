8fc6364d0b6c8aa6b7d4e7f6e8712e510ea634a8
cglenz <40736491+cglenz@users.noreply.github.com>
2020-03-10 11:12:08 +0400

    Create greater_than.py

---
+++ greater_than.py
@@ -0,0 +1,10 @@
+def larger(x,y):
+  if x != y:
+    if x>y:
+      print(x)
+      print(str(x)+" is larger than "+str(y))
+    else:
+      print(y)
+      print(str(y)+" is larger than "+str(x))  
+  else:
+    print("These two numbers are equal to each other")
